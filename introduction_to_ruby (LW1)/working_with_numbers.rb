63f9096688f1e9b40b615e1d0219d176863d6598
Alexander Sidorenko <xarendel@gmail.com>
2024-09-15 22:42:58 +0300

    Task 1.4.2. (Working with numbers: Count odd digits > 3)

--- introduction_to_ruby (LW1)/working_with_numbers.rb
+++ introduction_to_ruby (LW1)/working_with_numbers.rb
@@ -23,6 +23,19 @@ def sum_of_prime_divisors(number)
 end
 
 
+def count_odd_digits_greater_than_three(number)
+	count = 0
+	while number > 0
+		digit = number % 10
+		if digit > 3 and digit % 2 != 0
+			count += 1
+		end
+		number /= 10
+	end
+	return count
+end
+
+
 puts "\nEnter a number: "
 number = $stdin.gets.chomp
 
@@ -32,6 +45,7 @@ integer_as_string = integer_part.to_s
 if integer_as_string == number && integer_part != 0
 	puts "\n"
 	puts "The sum of the prime divisors of the number #{integer_part} is #{sum_of_prime_divisors(integer_part)}."
+	puts "The number of odd digits in #{integer_part} that are greater than 3 is #{count_odd_digits_greater_than_three(integer_part)}."
 else
 	puts "\n"
 	puts "The input is not a valid number."
