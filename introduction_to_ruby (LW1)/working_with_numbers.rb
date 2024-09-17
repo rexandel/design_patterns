bd10c39e12207b3addc70ce4455bf202b5922da0
Alexander Sidorenko <xarendel@gmail.com>
2024-09-18 00:31:05 +0300

    Task 1.4. (Working with numbers: Corrected logic)

--- introduction_to_ruby (LW1)/working_with_numbers.rb
+++ introduction_to_ruby (LW1)/working_with_numbers.rb
@@ -14,7 +14,7 @@ end
 
 def sum_of_prime_divisors(number)
 	total = 0
-	for divisor in 2..number
+	for divisor in 2...number
 		if number % divisor == 0 and is_prime?(divisor)
 			total += divisor
 		end
@@ -67,7 +67,7 @@ number = $stdin.gets.chomp
 integer_part = number.to_i
 integer_as_string = integer_part.to_s
 
-if integer_as_string == number && integer_part != 0
+if integer_as_string == number && integer_part != 0 && integer_part > 0
 	puts "\n"
 	puts "The sum of the prime divisors of the number #{integer_part} is #{sum_of_prime_divisors(integer_part)}."
 	puts "The number of odd digits in #{integer_part} that are greater than 3 is #{count_odd_digits_greater_than_three(integer_part)}."
