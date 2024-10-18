3067372da3db4920fae598725db780c4f0a68311
Alexander Sidorenko <xarendel@gmail.com>
2024-10-19 01:02:21 +0300

    Fixed a bug with indentation

--- student_classes/main.rb
+++ student_classes/main.rb
@@ -4,36 +4,40 @@ require_relative 'student_short.rb'
 
 begin
 	first_student = Student.new(
-					id: 1,
-					surname: "Sidorenko",
-					name: "Alexander",
-					patronymic: "Andreevich",
-					phone_number: "+7(989)-655-77-11",
-					telegram: "@rexandel",
-					git: "https://github.com/rexandel")
+		id: 1,
+		surname: "Sidorenko",
+		name: "Alexander",
+		patronymic: "Andreevich",
+		phone_number: "+7(989)-655-77-11",
+		telegram: "@rexandel",
+		git: "https://github.com/rexandel"
+	)
 
 	second_student = Student.new(
-					id: 2,
-					surname: "Ivanov",
-					name: "Ivan",
-					patronymic: "Ivanovich",
-					phone_number: "+7(977)-755-42-12",
-					telegram: "@ivan_develop",
-					email: "ivan_dev@gmail.com")
+		id: 2,
+		surname: "Ivanov",
+		name: "Ivan",
+		patronymic: "Ivanovich",
+		phone_number: "+7(977)-755-42-12",
+		telegram: "@ivan_develop",
+		email: "ivan_dev@gmail.com"
+	)
 
 	third_student = Student.new(
-					id: 3,
-					surname: "Petrov",
-					name: "Alexey",
-					patronymic: "Romanovich",
-					email: "alexey_2003@gmail.com",
-					git: "https://github.com/all_exay")
-	
+		id: 3,
+		surname: "Petrov",
+		name: "Alexey",
+		patronymic: "Romanovich",
+		email: "alexey_2003@gmail.com",
+		git: "https://github.com/all_exay"
+	)
+
 	fourth_student = Student.new(
-					id: 4,
-					surname: "Ivanov",
-					name: "Sergey",
-					patronymic: "Dmitrievich")
+		id: 4,
+		surname: "Ivanov",
+		name: "Sergey",
+		patronymic: "Dmitrievich"
+	)
 
 	puts first_student
 	puts second_student
--- student_classes/student_short.rb
+++ student_classes/student_short.rb
@@ -1,9 +1,9 @@
 require_relative 'person.rb'
 
 class StudentShort < Person
-    attr_reader :surname_and_initials
+	attr_reader :surname_and_initials
 
-    def initialize(input, id: nil)
+	def initialize(input, id: nil)
 		if input.is_a?(Student)
 			self.id = input.id
 			student_info = input.get_info
@@ -16,89 +16,89 @@ class StudentShort < Person
 		end
 	end
 
-    def get_any_contact
-        @contact
-    end
+	def get_any_contact
+		@contact
+	end
 
-    def validate_contacts?
-        !@contact.nil?
-    end
+	def validate_contacts?
+		!@contact.nil?
+	end
 
-    def self.valid_string_format?(input_string)
-        pattern = /^Surname and initials: .+ \| Git: .+ \| (Contact|Phone number|Telegram|Email): .+$/
-        input_string.match(pattern) != nil
-    end
+	def self.valid_string_format?(input_string)
+		pattern = /^Surname and initials: .+ \| Git: .+ \| (Contact|Phone number|Telegram|Email): .+$/
+		input_string.match(pattern) != nil
+	end
 
-    def self.valid_name?(surname_and_initials)
-        if surname_and_initials.match(/^[A-Za-z]/)
-            return surname_and_initials.match(/^[A-Za-z][a-z]+(?:[- ][A-Za-z][a-z]+)* \s*([A-Z]\. ?)+$/)
-        elsif surname_and_initials.match(/^[А-ЯЁ]/)
-            return surname_and_initials.match(/^[А-ЯЁ][а-яё]+(?:[- ][А-ЯЁ][а-яё]+)* \s*([А-ЯЁ]\. ?)+$/)
-        else
-            return false
-        end
-    end
+	def self.valid_name?(surname_and_initials)
+		if surname_and_initials.match(/^[A-Za-z]/)
+			return surname_and_initials.match(/^[A-Za-z][a-z]+(?:[- ][A-Za-z][a-z]+)* \s*([A-Z]\. ?)+$/)
+		elsif surname_and_initials.match(/^[А-ЯЁ]/)
+			return surname_and_initials.match(/^[А-ЯЁ][а-яё]+(?:[- ][А-ЯЁ][а-яё]+)* \s*([А-ЯЁ]\. ?)+$/)
+		else
+			return false
+		end
+	end
 
-    def to_s
-        "----------------------------------------\n" \
-        "ID: #{@id ? @id : 'Not specified'}\n" \
-        "Surname and Initials: #{@surname_and_initials}\n" \
-        "Git: #{@git ? @git : 'Not specified'}\n" \
-        "Contact: #{@contact ? @contact : 'Not specified'}\n" \
+	def to_s
+		"----------------------------------------\n" \
+		"ID: #{@id ? @id : 'Not specified'}\n" \
+		"Surname and Initials: #{@surname_and_initials}\n" \
+		"Git: #{@git ? @git : 'Not specified'}\n" \
+		"Contact: #{@contact ? @contact : 'Not specified'}\n" \
 		"Validation: #{self.validate? ? 'Passed' : 'Failed'}\n" \
-        "----------------------------------------"
-    end
+		"----------------------------------------"
+	end
 
-    private
+	private
 
-    def self.parse_string_surname_and_initials(input_string)
-        surname_and_initials_match = input_string.match(/Surname and initials: ([\w\s\.]+)/)
-        if surname_and_initials_match
-            return surname_and_initials_match[1].strip
-        else
-            raise ArgumentError, "Wrong string format"
-        end
-    end
+	def self.parse_string_surname_and_initials(input_string)
+		surname_and_initials_match = input_string.match(/Surname and initials: ([\w\s\.]+)/)
+		if surname_and_initials_match
+			return surname_and_initials_match[1].strip
+		else
+			raise ArgumentError, "Wrong string format"
+		end
+	end
 
-    def self.parse_string_git(input_string)
-        git_match = input_string.match(/Git: (https?:\/\/[\w\.\/\-]+|Not specified)/)
-        if git_match[1] == "Not specified"
-            return nil
-        elsif git_match
-            return git_match[1].strip
-        else
-            raise ArgumentError, "Wrong string format"
-        end
-    end
+	def self.parse_string_git(input_string)
+		git_match = input_string.match(/Git: (https?:\/\/[\w\.\/\-]+|Not specified)/)
+		if git_match[1] == "Not specified"
+			return nil
+		elsif git_match
+			return git_match[1].strip
+		else
+			raise ArgumentError, "Wrong string format"
+		end
+	end
 
-    def self.parse_string_contact(input_string)
-        contact_match = input_string.match(/(?:Contact|Phone number|Telegram|Email): ([^|]+|Not specified)/)
-        if contact_match[1] == "Not specified"
-            return nil
-        elsif contact_match
-            return contact_match[1].strip
-        else
-            raise ArgumentError, "Wrong string format"
-        end
-    end
+	def self.parse_string_contact(input_string)
+		contact_match = input_string.match(/(?:Contact|Phone number|Telegram|Email): ([^|]+|Not specified)/)
+		if contact_match[1] == "Not specified"
+			return nil
+		elsif contact_match
+			return contact_match[1].strip
+		else
+			raise ArgumentError, "Wrong string format"
+		end
+	end
 
-    def surname_and_initials=(surname_and_initials)
-        if self.class.valid_name?(surname_and_initials)
-            @surname_and_initials = surname_and_initials
-        else
-            raise ArgumentError, "Wrong surname and initials format."
-        end
-    end
-    
-    def set_contacts(contact)
-        if self.class.valid_phone_number?(contact) || self.class.valid_telegram?(contact) || self.class.valid_email?(contact)
-            @contact = contact
-        else
-            raise ArgumentError, "Wrong contact format."
-        end
-    end
+	def surname_and_initials=(surname_and_initials)
+		if self.class.valid_name?(surname_and_initials)
+			@surname_and_initials = surname_and_initials
+		else
+			raise ArgumentError, "Wrong surname and initials format."
+		end
+	end
 
-    def validate_and_parse_student_info(info)
+	def set_contacts(contact)
+		if self.class.valid_phone_number?(contact) || self.class.valid_telegram?(contact) || self.class.valid_email?(contact)
+			@contact = contact
+		else
+			raise ArgumentError, "Wrong contact format."
+		end
+	end
+
+	def validate_and_parse_student_info(info)
 		if self.class.valid_string_format?(info)
 			self.surname_and_initials = self.class.parse_string_surname_and_initials(info)
 			self.git = self.class.parse_string_git(info)
