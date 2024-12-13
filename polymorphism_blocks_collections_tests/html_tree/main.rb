0e588e421d2dd750e217e1296d881c060ab2cb7e
Alexander Sidorenko <xarendel@gmail.com>
2024-12-14 01:26:46 +0300

    Task 3.3.1 (Added method to read HTML from file)

---
+++ polymorphism_blocks_collections_tests/html_tree/main.rb
@@ -0,0 +1,26 @@
+class EmptyHTMLFileError < StandardError; end
+
+def read_html_file(file_path)
+	begin
+		File.open(file_path, "r") do |file|
+			html_string = file.read
+			if html_string.empty?
+				raise EmptyHTMLFileError, "The HTML file is empty."
+			end
+			return html_string
+		end
+	rescue => exception
+		puts "Error: #{exception.message}"
+		exit
+	end
+end
+
+begin
+	html_content = read_html_file("example.html")
+	puts "The content of the html file:"
+	puts html_content
+
+rescue => exception
+	puts "Error: #{exception.message}"
+	exit
+end
