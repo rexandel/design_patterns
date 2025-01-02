6ca6b74fd5d18e7eb69728adf576c8b5e8f0fdb5
Alexander Sidorenko <xarendel@gmail.com>
2025-01-02 16:03:43 +0300

    Task 3.4 (Refactor BinaryTree: improve logic for Comparable class instances)

--- student_classes/binary_tree.rb
+++ student_classes/binary_tree.rb
@@ -17,9 +17,8 @@ class BinaryTree
 		BinaryTreeIterator.new(self.root)
 	end
 
-	def initialize(&comparator)
+	def initialize
 		self.root = nil
-		@comparator = comparator || ->(a, b) { a <=> b }
 	end
 
 	def append(data)
@@ -32,12 +31,8 @@ class BinaryTree
 
 	private
 
-	def compare(a, b)
-		@comparator.call(a, b)
-	end
-
 	def append_node(prev_node, data)
-		if compare(data, prev_node.data) < 0
+		if data < prev_node.data
 			if prev_node.left.nil?
 				prev_node.left = TreeNode.new(data)
 				return
